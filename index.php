f4a588b86a14f341752cd28146827b29f9888da3
alexis <alexis@Alexis>
2014-11-07 12:33:50 +1000

    test done

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$request['act'] = 'login' ; 
+$request['email'] = 'apitest@ipayoptions.com'; 
+$request['password']  = '1q2w3e4r5t6y'; 
+$ch = curl_init('http://txfunds.uat.ipayoptions.com/api.php'); 
+$opts[CURLOPT_POST]  = true; $opts[CURLOPT_POSTFIELDS] = http_build_query($request); 
+$opts[CURLOPT_RETURNTRANSFER] = true; curl_setopt_array($ch,$opts); 
+$response = curl_exec($ch); 
+$response = json_decode($response,1); 
+
+//print_response($response) ;
+$details = serialize($response);
+echo $details . '<br /><br />';
+echo "status:" .$response['status'] . '<br />';
+echo "message:" .$response['msg'] . '<br />';
+echo "userid:" .$response['data']['userid'] . '<br />';
+echo "firstname:" .$response['data']['firstname'] . '<br />';
+echo "lastname:" .$response['data']['lastname'] . '<br />';
+echo "loginid: ".$response['data']['loginid']. '<br /><br />';
+
+//[10:46:36 AM] Andre Gelderblom: User: apitest@ipayoptions.com
+//Pass: 1q2w3e4r5t6y
+//URL: http://txfunds.uat.ipayoptions.com/
+
+$request['act'] = 'usercheck' ; 
+$request['mobile'] = '1221212121112'; 
+$request['country'] = 'AU'; 
+$request['loginid'] = $response['data']['loginid'];
+$ch = curl_init('http://txfunds.uat.ipayoptions.com/api.php'); 
+$opts[CURLOPT_POST]  = true; 
+$opts[CURLOPT_POSTFIELDS] = http_build_query($request); 
+$opts[CURLOPT_RETURNTRANSFER] = true; curl_setopt_array($ch,$opts); 
+$response = curl_exec($ch); 
+$response = json_decode($response,1); 
+
+$details = serialize($response);
+echo $details . '<br /><br />';
+echo "status:" .$response['status'] . '<br />';
+echo "message:" .$response['msg'] . '<br />';
+echo "userid:" .$response['data']['userid'] . '<br />';
+echo "status:" .$response['data']['status'] . '<br />';
+echo "accountid:" .$response['data']['accountid']['0'] . '<br />';
+ 
